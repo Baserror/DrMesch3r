a125ca796540f414b9911331f4cac0802a8eb72e
Baserror <barbeeker@gmail.com>
2019-01-09 15:43:27 +0100

    Drohnenzeug

---
+++ src/Drone/Drone.java
@@ -0,0 +1,18 @@
+package Drone;
+
+public class Drone {
+
+   /* public Map<String, Wheat> scanField() {
+        Map<String, Wheat> wheatHashSet = new HashMap<>();
+        for (int i = 0; i <= Field.getField().length; i++) {
+            for (int j = 0; j <= Field.getField().length; j++) {
+
+                wheatHashSet.put(String.valueOf(0)+String.valueOf(i)+String.valueOf(j),new Wheat(i,j));
+            }
+        }
+
+        return wheatHashSet;
+    }
+*/
+}
+
