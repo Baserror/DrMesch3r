f772a820a9060b066e36c57ad1b9ff499c45f54c
Baserror <barbeeker@gmail.com>
2019-01-09 20:10:40 +0100

    EditedMethod

--- src/Harvester/Engine.java
+++ src/Harvester/Engine.java
@@ -1,4 +1,40 @@
 package Harvester;
 
 public class Engine {
+    double speed;
+    boolean forwards;
+    boolean engineOn;
+
+    public void HarvesterOn(){
+        engineOn=true;
+        forwards=true;
+    }
+    public void HarvesterOff(){
+        engineOn=false;
+    }
+
+    public void speedPlus(){
+        speed += 1;
+    }
+
+    public void speedMinus(){
+        speed -= 1;
+    }
+
+    public void goForward(){
+        if (!forwards){
+            forwards=true;
+        }
+    }
+
+    public  void goBackward() {
+        if (forwards) {
+            forwards = false;
+        }
+    }
+
+
+
+
+
 }
